1e67dd7e495d258e0f7619e6641dafea01ca4eb6
Shahriar <shahriarh655@gmail.com>
2024-03-18 00:09:15 +0600

    Implemented redux persist to store the user informations to local storage

--- blog-client/src/main.jsx
+++ blog-client/src/main.jsx
@@ -5,12 +5,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Routes.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PersistGate persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
