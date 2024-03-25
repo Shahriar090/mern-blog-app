b2a4d3416170f7fbab83d91fa0c70ecc3cea44fd
Shahriar <shahriarh655@gmail.com>
2024-03-26 01:40:43 +0600

    added sidebar for dashboard

--- blog-client/src/pages/Dashboard.jsx
+++ blog-client/src/pages/Dashboard.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar/DashSidebar";
+import DashProfile from "../components/DashProfile/DashProfile";
+
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div className="h-screen">
-      <h1>Dashboard</h1>
-      <h1>Dashboard</h1>
-      <h1>Dashboard</h1>
-      <h1>Dashboard</h1>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="sidebar md:w-56">
+        <DashSidebar />
+      </div>
+      <div>{tab === "profile" && <DashProfile />}</div>
     </div>
   );
 }
