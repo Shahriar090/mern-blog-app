992f831194b3c377682f05109cb58da35e929c2f
Shahriar <shahriarh655@gmail.com>
2024-06-30 14:49:47 +0600

    Fetched blog posts data successfully

--- blog-client/src/components/DashPosts/DashPosts.jsx
+++ blog-client/src/components/DashPosts/DashPosts.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const DashPosts = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [userPosts, setUserPosts] = useState([]);
+  console.log(userPosts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    if (currentUser.isAdmin) {
+      fetchPosts();
+    }
+  }, [currentUser._id]);
   return (
     <div>
       <h1>Dash posts</h1>
