4787cbd0bc32f4637650d9bb2b54ed6e3074089e
Shahriar <shahriarh655@gmail.com>
2024-07-08 18:27:21 +0600

    Fix minor issue

--- blog-client/src/components/Header/Header.jsx
+++ blog-client/src/components/Header/Header.jsx
@@ -67,6 +67,9 @@ export default function Header() {
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
+        <Navbar.Link active={path === "/search"} as={"div"}>
+          <Link to="/search">All Blogs</Link>
+        </Navbar.Link>
         <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
